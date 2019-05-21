5b8a142af196a0fd3553724928380bc3c21648e1
Arturo Torres <efrainarturotorres@gmail.com>
2019-05-20 21:13:01 -0500

    stripe payment and checkout

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const errorController = require("./controllers/error");
 const mongoose = require("mongoose");
 const multer = require('multer');
@@ -10,7 +8,8 @@ const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
-
+const shopController = require("./controllers/shop");
+const isAuth = require("./middleware/is-auth");
 const User = require("./models/user");
 // const sequelize = require("./util/database");
 // const Product = require("./models/product");
@@ -77,13 +76,18 @@ app.use((req, res, next) => {
     .catch(err => { throw new Error(err); });
 });
 
-app.use(csrfProtection);
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
   next();
 });
 
+app.post("/create-order", isAuth, shopController.postOrder);
+app.use(csrfProtection);
+app.use((req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
--- controllers/shop.js
+++ controllers/shop.js
@@ -3,6 +3,7 @@ const path = require("path");
 const Product = require("../models/product");
 const Order = require("../models/order");
 const PDFDocument = require("pdfkit");
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
 
 const ITEMS_PER_PAGE = 2;
 
@@ -112,10 +113,15 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  const token = req.body.stripeToken;
+  let totalSum = 0;
   req.user
     .populate("cart.items.productId")
     .execPopulate()
     .then(user => {
+      user.cart.items.forEach(p => {
+        totalSum += p.quantity * p.productId.price;
+      });
       const products = user.cart.items.map(i => {
         return { quantity: i.quantity, product: { ...i.productId._doc } };
       });
@@ -129,6 +135,13 @@ exports.postOrder = (req, res, next) => {
       return order.save();
     })
     .then(result => {
+      const charge = stripe.charges.create({
+        amount: totalSum * 100,
+        currency: 'usd',
+        description: 'Example',
+        source: token,
+        metadata: { order_id: result._id.toString() }
+      });
       return req.user.clearCart();
     })
     .then(() => {
@@ -150,10 +163,22 @@ exports.getOrders = (req, res, next) => {
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout"
-  });
+  req.user
+    .populate("cart.items.productId")
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
+      let total = 0;
+      products.forEach(p => {
+        total += p.quantity * p.productId.price;
+      });
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum: total
+      });
+    })
 };
 
 exports.getInvoice = (req, res, next) => {
